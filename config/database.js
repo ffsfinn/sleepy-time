aa612fcad235140cb1571b4ef8012d235cff673d
Finn <anthony.torres0926@gmail.com>
2019-11-25 10:45:33 -0700

    Connected to MongoDB

---
+++ config/database.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost',{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on('connected', function(){
+    console.log(`Connected to MongoDB on host:${db.host} and port:${db.port}`)
+});
