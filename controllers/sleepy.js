ffc57a5d2c796e72d5f5a164e5c94abccaba9a65
Finn <anthony.torres0926@gmail.com>
2019-12-02 01:35:08 -0700

    A Little More User Friendly

--- controllers/sleepy.js
+++ controllers/sleepy.js
@@ -1,11 +1,44 @@
-const sleepTime = require('../models/sleep');
+const Sleeptime = require('../models/sleep');
+const Sleepuser = require('../models/user');
 
 module.exports = {
-    index
+    index,
+    new: newTime,
+    create,
+    show
 };
 
 function index(req, res) {
-    res.render('sleepy', {
+    Sleeptime.find({}, function(err, time) {
+        res.render('sleepy/main', {
+            user: req.user,
+            time
+        });
+    });
+}
+
+function newTime(req, res) {
+    res.render('sleepy/new', {
         user: req.user
+    });
+}
+
+function create(req, res) {
+    res.render('sleepy/new', {
+        user: req.user
+    });
+    let time = new Sleeptime(req.body);
+    time.save(function(err){
+        if(err) return res.render('sleepy/new');
+        res.redirect('sleepy');
+    });
+}
+
+function show(req, res) {
+    Sleeptime.findById(req.params.id, function(err, time){
+        res.render('sleepy/show', {
+            user: req.user,
+            time
+        });
     });
 }
--- routes/sleepy.js
+++ routes/sleepy.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const sleepTime = require('../controllers/sleepy')
 
-router.get('/', sleepTime.index)
+router.get('/', sleepTime.index);
+router.post('/', sleepTime.create);
+router.get('/new', sleepTime.new);
+router.get('/:id', sleepTime.show);
 
 module.exports = router;
